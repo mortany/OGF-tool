8aed27a9687514587b89ef4d124e50fb68c657e9
Mortan <cheatmaster1@mail.ru>
2022-01-12 10:42:17 +0300

    Init.
    Добавьте файлы проекта.

---
+++ OGF tool/Form1.cs
@@ -0,0 +1,351 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OGF_tool
+{
+
+	public partial class Form1 : Form
+	{
+
+		// File sytem
+		public OGF_Children OGF_V = null;
+		public List<byte> file_bytes = new List<byte>();
+		public string FILE_NAME = "";
+
+		// Input
+		public bool bKeyIsDown = false;
+
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			//foreach (var box in textBoxes)
+			//tabPage1.Controls.Add(box);
+
+			if (Environment.GetCommandLineArgs().Length > 1)
+			{
+				OpenFile(Environment.GetCommandLineArgs()[1]);
+				FILE_NAME = Environment.GetCommandLineArgs()[1];
+				AfterLoad();
+			}
+
+		}
+
+		private void CreateGroupBox(int idx)
+		{
+			var GroupBox = new GroupBox();
+			GroupBox.Location = new System.Drawing.Point(3, 3 + 126 * idx);
+			GroupBox.Size = new System.Drawing.Size(366, 126);
+			GroupBox.Text = "Set: [" + idx + "]";
+			GroupBox.Name = "GrpBox_" + idx;
+			CreateBoxes(idx, GroupBox);
+			CreateLabels(idx, GroupBox);
+			tabPage1.Controls.Add(GroupBox);
+		}
+
+		private void CreateBoxes(int idx, GroupBox box)
+		{
+			var newTextBox = new TextBox();
+			newTextBox.Name = "textureBox_" + idx;
+			newTextBox.Size = new System.Drawing.Size(355, 23);
+			//newTextBox.Location = new System.Drawing.Point(56,30+88*idx + 15* idx);
+			newTextBox.Location = new System.Drawing.Point(6, 39);
+			newTextBox.TextChanged += new System.EventHandler(this.TextBoxFilter);
+
+			var newTextBox2 = new TextBox();
+			newTextBox2.Name = "shaderBox_" + idx;
+			newTextBox2.Size = new System.Drawing.Size(355, 23);
+			//newTextBox2.Location = new System.Drawing.Point(56, 74 + 88* idx + 15 * idx);
+			newTextBox2.Location = new System.Drawing.Point(6, 88);
+			newTextBox2.TextChanged += new System.EventHandler(this.TextBoxFilter);
+			box.Controls.Add(newTextBox);
+			box.Controls.Add(newTextBox2);
+		}
+
+		private void CreateLabels(int idx, GroupBox box)
+		{
+			var newLbl = new Label();
+			newLbl.Name = "textureLbl_" + idx;
+			newLbl.Text = "Texture Path:";
+			//newLbl.Size = new System.Drawing.Size(75, 15);
+			//newLbl.Location = new System.Drawing.Point(56, 12 + 88*idx + 15 * idx);
+			newLbl.Location = new System.Drawing.Point(6, 21);
+
+			var newLbl2 = new Label();
+			newLbl2.Name = "shaderLbl_" + idx;
+			newLbl2.Text = "Shader Name:";
+			//newLbl2.Size = new System.Drawing.Size(81, 15);
+			//newLbl2.Location = new System.Drawing.Point(56, 56 + 88 *idx + 15 * idx);
+			newLbl2.Location = new System.Drawing.Point(6, 70);
+
+			//var newLbl3 = new Label();
+			//newLbl3.Name = "textureSetLbl_" + idx;
+			//newLbl3.Text = "Set: [" + idx +"]";
+			//newLbl3.Location = new System.Drawing.Point(0, 44 + 88 * idx + 15 * idx);
+
+			box.Controls.Add(newLbl);
+			box.Controls.Add(newLbl2);
+			//box.Controls.Add(newLbl3);
+		}
+
+		private void Clear()
+		{
+			FILE_NAME = "";
+			OGF_V = null;
+			file_bytes.Clear();
+			tabPage1.Controls.Clear();
+			richTextBox1.Clear();
+		}
+
+		private void AfterLoad()
+		{
+			for (int i = 0; i < OGF_V.childs.Count; i++)
+			{
+				CreateGroupBox(i);
+
+				var box = tabPage1.Controls["GrpBox_" + i];
+
+				if (box != null)
+				{
+					var Cntrl = box.Controls["textureBox_" + i];
+					Cntrl.Text = OGF_V.childs[i].m_texture;
+					var Cntrl2 = box.Controls["shaderBox_" + i];
+					Cntrl2.Text = OGF_V.childs[i].m_shader;
+				}
+			}
+
+			if (OGF_V.refs.refs0 == null) return;
+
+			richTextBox1.Lines = OGF_V.refs.refs0.ToArray();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			openFileDialog1.FileName = "";
+			DialogResult res = openFileDialog1.ShowDialog();
+
+			if (res == DialogResult.OK)
+			{
+				Clear();
+				FILE_NAME = openFileDialog1.FileName;
+				OpenFile(FILE_NAME);
+				AfterLoad();
+			}
+		}
+
+		private void CopyParams()
+		{
+			if (richTextBox1.Lines.Count() > 0)
+			{
+				OGF_V.refs.refs0 = new List<string>();
+				OGF_V.refs.refs0.AddRange(richTextBox1.Lines);
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (FILE_NAME == "") return;
+
+			CopyParams();
+			SaveFile(FILE_NAME);
+		}
+
+		private void SaveFile(string filename)
+		{
+			file_bytes.Clear();
+			using (var fileStream = new BinaryReader(File.OpenRead(filename)))
+			{
+				byte[] temp = fileStream.ReadBytes((int)(OGF_V.pos));
+				file_bytes.AddRange(temp);
+
+				uint chld_section = fileStream.ReadUInt32();
+				uint new_size = fileStream.ReadUInt32();
+
+				foreach (var ch in OGF_V.childs)
+				{
+					new_size += ch.NewSize();
+				}
+
+				file_bytes.AddRange(BitConverter.GetBytes(chld_section));
+				file_bytes.AddRange(BitConverter.GetBytes(new_size));
+
+				foreach (var ch in OGF_V.childs)
+				{
+					temp = fileStream.ReadBytes((int)(ch.parent_pos - fileStream.BaseStream.Position));
+					file_bytes.AddRange(temp);
+					fileStream.ReadUInt32();
+					new_size = fileStream.ReadUInt32();
+					new_size += ch.NewSize();
+					file_bytes.AddRange(BitConverter.GetBytes(ch.parent_id));
+					file_bytes.AddRange(BitConverter.GetBytes(new_size));
+
+					temp = fileStream.ReadBytes((int)(ch.pos - fileStream.BaseStream.Position));
+					file_bytes.AddRange(temp);
+					file_bytes.AddRange(ch.data());
+					fileStream.BaseStream.Position += ch.old_size + 8;
+				}
+
+				if (OGF_V.refs.pos > 0)
+					temp = fileStream.ReadBytes((int)(OGF_V.refs.pos - fileStream.BaseStream.Position));
+				else
+					temp = fileStream.ReadBytes((int)(fileStream.BaseStream.Length - fileStream.BaseStream.Position));
+
+				file_bytes.AddRange(temp);
+
+				if (OGF_V.refs.refs0 != null)
+				{
+
+					if (OGF_V.refs.refs0.Count() > 1)
+					{
+						file_bytes.AddRange(BitConverter.GetBytes((uint)OGF.OGF4_S_MOTION_REFS_1));
+						file_bytes.AddRange(BitConverter.GetBytes(OGF_V.refs.chunk_size()));
+						file_bytes.AddRange(OGF_V.refs.count());
+					}
+					else
+					{
+						file_bytes.AddRange(BitConverter.GetBytes((uint)OGF.OGF4_S_MOTION_REFS_0));
+						file_bytes.AddRange(BitConverter.GetBytes(OGF_V.refs.chunk_size()));
+					}
+
+					file_bytes.AddRange(OGF_V.refs.data());
+				}
+			}
+
+			using (var fileStream = new FileStream(filename, FileMode.Truncate))
+			{
+				byte[] data = file_bytes.ToArray();
+				fileStream.Write(data, 0, data.Length);
+			}
+		}
+
+		private void TextBoxFilter(object sender, EventArgs e)
+		{
+
+			TextBox curBox = sender as TextBox;
+
+			if (bKeyIsDown)
+			{
+				string mask = @"^[A-Za-z0-9_$]*$";
+				Match match = Regex.Match(curBox.Text, mask);
+				if (!match.Success)
+				{
+					int temp = curBox.SelectionStart;
+					curBox.Text = curBox.Text.Remove(curBox.SelectionStart - 1, 1);
+					curBox.SelectionStart = temp - 1;
+				}
+			}
+
+			bKeyIsDown = false;
+
+			string currentField = curBox.Name.ToString().Split('_')[0];
+			int idx = Convert.ToInt32(curBox.Name.ToString().Split('_')[1]);
+
+			switch (currentField)
+			{
+				case "textureBox": OGF_V.childs[idx].m_texture = curBox.Text; break;
+				case "shaderBox": OGF_V.childs[idx].m_shader = curBox.Text; break;
+
+			}
+		}
+		private void OpenFile(string filename)
+		{
+			var xr_loader = new XRayLoader();
+
+			OGF_V = new OGF_Children();
+
+			using (var r = new BinaryReader(File.OpenRead(filename)))
+			{
+
+				xr_loader.SetStream(r.BaseStream);
+
+				uint size = xr_loader.find_chunkSize((int)OGF.OGF_HEADER);
+
+				xr_loader.ReadByte(); // Skip m_version
+				byte model_type = xr_loader.ReadByte();
+
+				xr_loader.ReadBytes((int)size - 2);
+
+				//MessageBox.Show("Model type is: "+ model_type);
+
+				if (!xr_loader.find_chunk((int)OGF.OGF4_S_DESC)) return;
+
+				string m_source = xr_loader.read_stringZ();
+				string m_export_tool = xr_loader.read_stringZ();
+				uint m_export_time = xr_loader.ReadUInt32();
+				string m_owner_name = xr_loader.read_stringZ();
+				uint m_creation_time = xr_loader.ReadUInt32();
+				string m_exportm_modif_name_tool = xr_loader.read_stringZ();
+				uint m_modified_time = xr_loader.ReadUInt32();
+				//System.DateTime dt_e = new System.DateTime(1970, 1, 1).AddSeconds(m_export_time);
+				//System.DateTime dt_c = new System.DateTime(1970, 1, 1).AddSeconds(m_creation_time);
+				//System.DateTime dt_m = new System.DateTime(1970, 1, 1).AddSeconds(m_modified_time);
+				//MessageBox.Show("Source: " + m_source + "\n" +
+				//	"Export Tool: " + m_export_tool + "\n" +
+				//	"Owner Name: " + m_owner_name + "\n" +
+				//	"Export modifed Tool:" + m_exportm_modif_name_tool);
+				//MessageBox.Show("Export Time: " + dt_e +"\n" + 
+				//	"Creation Time: " + dt_c + "\n"+
+				//	"Modified Time: " + dt_m);
+
+
+
+				if (!xr_loader.SetData(xr_loader.find_and_return_chunk_in_chunk((int)OGF.OGF4_CHILDREN, false, true))) return;
+
+				OGF_V.pos = xr_loader.chunk_pos;
+
+				int id = 0;
+
+				while (true)
+				{
+
+					if (!xr_loader.find_chunk(id)) break;
+
+					Stream temp = xr_loader.reader.BaseStream;
+
+					if (!xr_loader.SetData(xr_loader.find_and_return_chunk_in_chunk(id, false, true))) break;
+
+					long pos = xr_loader.chunk_pos + OGF_V.pos + 16;
+					size = xr_loader.find_chunkSize((int)OGF.OGF4_TEXTURE);
+
+					if (size == 0) break;
+
+					OGF_Child chld = new OGF_Child(xr_loader.chunk_pos + pos, id, pos - 8, (int)size, xr_loader.read_stringZ(), xr_loader.read_stringZ());
+					OGF_V.childs.Add(chld);
+
+					id++;
+					xr_loader.SetStream(temp);
+
+				}
+
+				xr_loader.SetStream(r.BaseStream);
+
+				bool v3 = false;
+
+				if (v3 = !xr_loader.find_chunk((int)OGF.OGF4_S_MOTION_REFS_1, false, true))
+					if (!xr_loader.find_chunk((int)OGF.OGF4_S_MOTION_REFS_0, false, true))
+						return;
+
+				OGF_V.refs.pos = xr_loader.chunk_pos;
+				OGF_V.refs.refs0 = new List<string>();
+
+				if (v3) { OGF_V.refs.refs0.Add(xr_loader.read_stringZ()); return; }
+
+				uint count = xr_loader.ReadUInt32();
+
+				for (int i = 0; i < count; i++)
+					OGF_V.refs.refs0.Add(xr_loader.read_stringZ());
+
+			}
+		}
+	}
+}
---
+++ OGF tool/OGF.cs
@@ -0,0 +1,336 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace OGF_tool
+{
+    public enum OGF
+    {
+        OGF_HEADER = 0x1,
+
+        //build 729
+        OGF2_TEXTURE = 0x2,
+        OGF2_TEXTURE_L = 0x3,
+        OGF2_BBOX = 0x6,
+        OGF2_VERTICES = 0x7,
+        OGF2_INDICES = 0x8,
+        OGF2_VCONTAINER = 0xb,
+        OGF2_BSPHERE = 0xc,
+
+        OGF3_TEXTURE = 0x2,
+        OGF3_TEXTURE_L = 0x3,
+        OGF3_CHILD_REFS = 0x5,
+        OGF3_BBOX = 0x6,
+        OGF3_VERTICES = 0x7,
+        OGF3_INDICES = 0x8,
+        OGF3_LODDATA = 0x9, // not sure
+        OGF3_VCONTAINER = 0xa,
+        OGF3_BSPHERE = 0xb,
+        OGF3_CHILDREN_L = 0xc,
+        OGF3_S_BONE_NAMES = 0xd,
+        OGF3_S_MOTIONS = 0xe,// build 1469 - 1580
+        OGF3_DPATCH = 0xf,  // guessed name
+        OGF3_LODS = 0x10,   // guessed name
+        OGF3_CHILDREN = 0x11,
+        OGF3_S_SMPARAMS = 0x12,// build 1469
+        OGF3_ICONTAINER = 0x13,// build 1865
+        OGF3_S_SMPARAMS_NEW = 0x14,// build 1472 - 1865
+        OGF3_LODDEF2 = 0x15,// build 1865
+        OGF3_TREEDEF2 = 0x16,// build 1865
+        OGF3_S_IKDATA_0 = 0x17,// build 1475 - 1580
+        OGF3_S_USERDATA = 0x18,// build 1537 - 1865
+        OGF3_S_IKDATA = 0x19,// build 1616 - 1829, 1844
+        OGF3_S_MOTIONS_NEW = 0x1a,// build 1616 - 1865
+        OGF3_S_DESC = 0x1b,// build 1844
+        OGF3_S_IKDATA_2 = 0x1C,// build 1842 - 1865
+        OGF3_S_MOTION_REFS = 0x1D,// build 1842
+
+        OGF4_TEXTURE = 0x2,
+        OGF4_VERTICES = 0x3,
+        OGF4_INDICES = 0x4,
+        OGF4_P_MAP = 0x5,
+        OGF4_SWIDATA = 0x6,
+        OGF4_VCONTAINER = 0x7,
+        OGF4_ICONTAINER = 0x8,
+        OGF4_CHILDREN = 0x9,
+        OGF4_CHILDREN_L = 0xa,
+        OGF4_LODDEF2 = 0xb,
+        OGF4_TREEDEF2 = 0xc,
+        OGF4_S_BONE_NAMES = 0xd,
+        OGF4_S_MOTIONS = 0xe,
+        OGF4_S_SMPARAMS = 0xf,
+        OGF4_S_IKDATA = 0x10,
+        OGF4_S_USERDATA = 0x11,
+        OGF4_S_DESC = 0x12,
+        OGF4_S_MOTION_REFS_0 = 0x13,    // pre-CS format
+        OGF4_SWICONTAINER = 0x14,
+        OGF4_GCONTAINER = 0x15,
+        OGF4_FASTPATH = 0x16,
+        OGF4_S_LODS = 0x17,
+        OGF4_S_MOTION_REFS_1 = 0x18,    // introduced in clear sky
+    };
+    public class XRayLoader
+    {
+        public long chunk_pos = 0;
+
+        uint CHUNK_COMPRESSED = 0x80000000;
+
+        public MemoryStream mem_stream;
+        public BinaryReader reader;
+
+
+        public void Destroy()
+        {
+            mem_stream.Dispose();
+            reader.Dispose();
+        }
+
+        public byte ReadByte()
+        {
+            return reader.ReadByte();
+        }
+
+        public int ReadInt32()
+        {
+            return reader.ReadInt32();
+        }
+
+        public float ReadFloat()
+        {
+            return reader.ReadSingle();
+        }
+
+        public uint ReadUInt32()
+        {
+            return reader.ReadUInt32();
+        }
+
+        public byte[] ReadBytes(int count)
+        {
+            return reader.ReadBytes(count);
+        }
+
+        public bool SetData(byte[] input)
+        {
+            if (input == null) return false;
+            mem_stream = new MemoryStream(input);
+            reader = new BinaryReader(mem_stream);
+            return true;
+        }
+
+        public void SetStream(Stream stream)
+        {
+            reader = new BinaryReader(stream);
+        }
+
+        public void SetReader(BinaryReader rd)
+        {
+            reader = rd;
+        }
+
+        public bool find_chunk(int chunkId, bool skip = false, bool reset = false)
+        {
+            return find_chunkSize(chunkId, skip, reset) != 0;
+        }
+
+        public byte[] find_and_return_chunk_in_chunk(int chunkId, bool skip = false, bool reset = false)
+        {
+            int size = (int)find_chunkSize(chunkId, skip, reset);
+
+            if (size > 0)
+            {
+                return ReadBytes(size);
+            }
+            else
+                return null;
+        }
+
+        public uint find_chunkSize(int chunkId, bool skip = false, bool reset = false)
+        {
+            chunk_pos = 0;
+
+            if (reset) reader.BaseStream.Position = 0;
+
+            while (reader.BaseStream.Position < reader.BaseStream.Length)
+            {
+                if (reader.BaseStream.Position + 8 > reader.BaseStream.Length)
+                    return 0;
+
+                uint dwType = reader.ReadUInt32();
+                uint dwSize = reader.ReadUInt32();
+
+                if (dwType == chunkId || (dwType ^ CHUNK_COMPRESSED) == chunkId)
+                {
+                    chunk_pos = reader.BaseStream.Position - 8;
+                    return dwSize;
+                }
+                else
+                {
+                    if (reader.BaseStream.Position + dwSize < reader.BaseStream.Length)
+                        reader.BaseStream.Position += dwSize;
+                    else if (reader.BaseStream.Position + 8 < reader.BaseStream.Length)
+                        reader.BaseStream.Position += 4;
+                    else
+                        return 0;
+                }
+            }
+
+            return 0;
+        }
+
+        public void open_chunk(BinaryWriter w, int chunkId)
+        {
+            w.Write(chunkId);
+            chunk_pos = w.BaseStream.Position;
+            w.Write(0);     // the place for 'size'
+        }
+
+        public void close_chunk(BinaryWriter w)
+        {
+            if (chunk_pos == 0)
+            {
+                throw new InvalidOperationException("no chunk!");
+            }
+
+            long pos = w.BaseStream.Position;
+            w.BaseStream.Position = chunk_pos;
+            w.Write((int)(pos - chunk_pos - 4));
+            w.BaseStream.Position = pos;
+            chunk_pos = 0;
+        }
+
+        public string read_stringZ()
+        {
+            List<char> str = new List<char>();
+
+            while (reader.BaseStream.Position < reader.BaseStream.Length)
+            {
+                byte one = reader.ReadByte();
+                if (one != 0)
+                {
+                    str.Add((char)one);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return new string(str.ToArray());
+        }
+
+        public void write_stringZ(BinaryWriter w, string str)
+        {
+            foreach (char c in str)
+            {
+                byte b = (byte)c;
+                w.Write(b);
+            }
+
+            w.Write((byte)0);
+        }
+    }
+
+    public class OGF_Children
+    {
+        public List<OGF_Child> childs = new List<OGF_Child>();
+
+        public uint chunk_size;
+
+        public long pos;
+
+        public MotionRefs refs = new MotionRefs();
+
+        public OGF_Children()
+        {
+            refs.pos = -1;
+        }
+
+    }
+
+    public struct MotionRefs
+    {
+        public long pos;
+        public List<string> refs0;
+        public uint chunk_size()
+        {
+            uint temp = 4;
+            foreach (var text in refs0)
+                temp += (uint)text.Length + 1;
+            return temp;
+        }
+        public byte[] count()
+        {
+            return BitConverter.GetBytes(refs0.Count);
+        }
+        public byte[] data()
+        {
+            List<byte> temp = new List<byte>();
+
+            foreach (var str in refs0)
+            {
+                temp.AddRange(Encoding.ASCII.GetBytes(str));
+                temp.Add(0);
+            }
+
+            return temp.ToArray();
+        }
+    }
+
+    public class OGF_Child
+    {
+        public string m_texture;
+
+        public string m_shader;
+
+        public OGF_Child(long _pos, int _parent_id, long _parent_pos, int _old_size, string texture, string shader)
+        {
+            pos = _pos;
+            parent_id = _parent_id;
+            parent_pos = _parent_pos;
+            m_texture = texture;
+            m_shader = shader;
+            old_size = _old_size;
+        }
+
+        public long pos;
+
+        public long parent_pos;
+        public int parent_id;
+
+        public int old_size;
+
+        public uint NewSize()
+        {
+            return (uint)(m_texture.Length + m_shader.Length + 2 - old_size);
+        }
+
+        public byte[] data()
+        {
+            List<byte> temp = new List<byte>();
+
+            temp.AddRange(BitConverter.GetBytes(2));
+            temp.AddRange(BitConverter.GetBytes(m_texture.Length + m_shader.Length + 2));
+
+            foreach (char c in m_texture)
+            {
+                byte b = (byte)c;
+                temp.Add(b);
+            }
+
+            temp.Add(0);
+
+            foreach (char c in m_shader)
+            {
+                byte b = (byte)c;
+                temp.Add(b);
+            }
+
+            temp.Add(0);
+
+            return temp.ToArray();
+
+        }
+    }
+}
