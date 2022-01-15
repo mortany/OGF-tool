e28d653018ae258f4ff4bc573fb929cb708c0da5
Mortan <cheatmaster1@mail.ru>
2022-01-15 13:59:18 +0300

    Fix crash

--- OGF tool/Form1.cs
+++ OGF tool/Form1.cs
@@ -128,7 +128,8 @@ namespace OGF_tool
 
 			richTextBox1.Lines = OGF_V.refs.refs0.ToArray();
 
-            richTextBox2.Text = OGF_V.usertdata.data;
+            if(OGF_V.usertdata!=null)
+                richTextBox2.Text = OGF_V.usertdata.data;
 
         }
 
